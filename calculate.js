b37274cdb1d29f2474a1473453ef3c0dff892dd2
AidarMuhametzianov <aldarmuha1992@gmail.com>
2025-02-02 21:01:25 +0500

    7. Разработал простой калькулятор

---
+++ calculate.js
@@ -0,0 +1,12 @@
+const simpleCalculate = function (num1, num2, operation) {
+    switch (operation) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            return num1 / num2;
+    }
+}
